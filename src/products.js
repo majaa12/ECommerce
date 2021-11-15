7910755c257c81299ef20a02c08b18212ddcba0b
majaa12 <majacol98@gmail.com>
2021-11-15 23:31:34 +0100

    Inicijalizacija git repozitorijuma, dodat Bootstrap, Header, Footer i HomeScreen

---
+++ src/products.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+      '_id': '1',
+      'name': 'Strength bands',
+      'image': '/images/strengthbands.jpg',
+      'description':
+        'Strength Bands are versatile tools for adding resistance to stretching, lifting, and bodyweight training exercises. All bands are made of 100% natural latex through a continuous layering process to ensure long life. All bands are 41" in length and ~3/8" thick.',
+      'brand': 'Again faster',
+      'category': 'Fitness gear',
+      'price': 24.99,
+      'countInStock': 10,
+      'rating': 4.5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '2',
+      'name': 'Foam roller',
+      'image': '/images/foamroller.jpg',
+      'description':
+        'Foam rollers are also be used for physical therapy, training and conditioning, core strength and stability, stretching, postural alignment, and balance exercises.',
+      'brand': 'Lonsdale',
+      'category': 'Fitness gear',
+      'price': 34.99,
+      'countInStock': 7,
+      'rating': 4.0,
+      'numReviews': 8,
+    },
+    {
+      '_id': '3',
+      'name': 'Yoga mat',
+      'image': '/images/yogamat.jpg',
+      'description':
+        'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      'brand': 'Champion',
+      'category': 'Fitness gear',
+      'price': 29.99,
+      'countInStock': 5,
+      'rating': 3,
+      'numReviews': 12,
+    },
+    {
+      '_id': '4',
+      'name': 'Kettlebell',
+      'image': '/images/kettlebell.jpg',
+      'description':
+        'Kettlebell weights are one of the simplest and yet most broadly functional pieces of strength training equipment. Perfect for building strength, stability, and explosiveness, kettlebell exercises train core to extremity motor patterns applicable to every athlete.',
+      'brand': 'Again faster',
+      'category': 'Fitness gear',
+      'price': 44.99,
+      'countInStock': 11,
+      'rating': 5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '5',
+      'name': 'Dumbbells',
+      'image': '/images/dumbbells.jpg',
+      'description':
+        'Dumbbells offer one of the most effective ways to add variance to any training program. Functional dumbbell movements cultivate stability, coordination and strength in ways that cannot be achieved using a barbell.',
+      'brand': 'Everlast',
+      'category': 'Fitness gear',
+      'price': 69.99,
+      'countInStock': 7,
+      'rating': 3.5,
+      'numReviews': 10,
+    },
+    {
+      '_id': '6',
+      'name': 'Bench',
+      'image': '/images/bench.jpg',
+      'description':
+        'An essential for every commercial and home gym, the Flat Bench is simple, effective, and built like a tank. Weight tested to 1,000 pounds, so you can push your limits without worrying about the bench\'s stability.',
+      'brand': 'Techno gym',
+      'category': 'Fitness gear',
+      'price': 179.99,
+      'countInStock': 0,
+      'rating': 4,
+      'numReviews': 12,
+    },
+  ]
+  
+  
+  export default products
